8c5d9bdb3ae332ad9e5ed6546034c27435e7ca74
Fabian Marin <fabian@conexus202.com>
2014-11-19 18:13:17 -0500

    first commit

---
+++ PruebMvcWebApi/Controllers/PersonasController.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+using PruebMvcWebApi.Models;
+
+namespace PruebMvcWebApi.Controllers
+{
+    public class PersonasController : ApiController
+    {
+        private DataBaseContext db = new DataBaseContext();
+
+        // GET api/Personas
+        public IEnumerable<Persona> GetPersonas()
+        {
+            return db.Personas.AsEnumerable();
+        }
+
+        // GET api/Personas/5
+        public Persona GetPersona(int id)
+        {
+            Persona persona = db.Personas.Find(id);
+            if (persona == null)
+            {
+                throw new HttpResponseException(Request.CreateResponse(HttpStatusCode.NotFound));
+            }
+
+            return persona;
+        }
+
+        // PUT api/Personas/5
+        public HttpResponseMessage PutPersona(int id, Persona persona)
+        {
+            if (!ModelState.IsValid)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ModelState);
+            }
+
+            if (id != persona.Id)
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest);
+            }
+
+            db.Entry(persona).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.NotFound, ex);
+            }
+
+            return Request.CreateResponse(HttpStatusCode.OK);
+        }
+
+        // POST api/Personas
+        public HttpResponseMessage PostPersona(Persona persona)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Personas.Add(persona);
+                db.SaveChanges();
+
+                HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.Created, persona);
+                response.Headers.Location = new Uri(Url.Link("DefaultApi", new { id = persona.Id }));
+                return response;
+            }
+            else
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ModelState);
+            }
+        }
+
+        // DELETE api/Personas/5
+        public HttpResponseMessage DeletePersona(int id)
+        {
+            Persona persona = db.Personas.Find(id);
+            if (persona == null)
+            {
+                return Request.CreateResponse(HttpStatusCode.NotFound);
+            }
+
+            db.Personas.Remove(persona);
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.NotFound, ex);
+            }
+
+            return Request.CreateResponse(HttpStatusCode.OK, persona);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ PruebMvcWebApi/Models/DataBaseContext.cs
@@ -0,0 +1,22 @@
+﻿using System.Data.Entity;
+
+namespace PruebMvcWebApi.Models
+{
+    public class DataBaseContext : DbContext
+    {
+        // You can add custom code to this file. Changes will not be overwritten.
+        // 
+        // If you want Entity Framework to drop and regenerate your database
+        // automatically whenever you change your model schema, add the following
+        // code to the Application_Start method in your Global.asax file.
+        // Note: this will destroy and re-create your database with every model change.
+        // 
+        // System.Data.Entity.Database.SetInitializer(new System.Data.Entity.DropCreateDatabaseIfModelChanges<PruebMvcWebApi.Models.DataBaseContext>());
+
+        public DataBaseContext() : base("name=DataBaseContext")
+        {
+        }
+
+        public DbSet<Persona> Personas { get; set; }
+    }
+}
